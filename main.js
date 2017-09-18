9b4cded4220e7b5d72503a65aac4987739b2baf7
sivmic <sivak.dacice@outlook.com>
2017-09-18 21:12:20 +0200

    Additional menu functions added

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ let mainWindow = null;
 function createWindow () {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 600,
+        height: 400,
         minWidth: 600,
         minHeight: 400,
         icon: './cpu.png',
--- src/js/menu.js
+++ src/js/menu.js
@@ -4,22 +4,22 @@ const template = [
     {
         label: 'Edit',
         submenu: [
-            {role: 'undo', accelerator: 'CmdOrCtrl+Z'},
-            {role: 'redo', accelerator: 'Shift+CmdOrCtrl+Z'}
+            {role: 'undo', label: 'Undo', accelerator: 'CmdOrCtrl+Z'},
+            {role: 'redo', label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z'}
         ]
     },
     {
         label: 'View',
         submenu: [
-            {role: 'reload', accelerator: 'CmdOrCtrl+R'},
-            {role: 'toggledevtools', accelerator: 'Alt+CmdOrCtrl+I'}
+            {role: 'reload', label: 'Reload', accelerator: 'CmdOrCtrl+R'},
+            {role: 'toggledevtools', label: 'Toggle DevTools', accelerator: 'Alt+CmdOrCtrl+I'}
         ]
     },
     {
         role: 'window',
         submenu: [
-            {role: 'close', accelerator: 'CmdOrCtrl+W'},
-            {role: 'minimize', accelerator: 'CmdOrCtrl+M'}
+            {role: 'close', label: 'Close', accelerator: 'CmdOrCtrl+W'},
+            {role: 'minimize', label: 'Minimize', accelerator: 'CmdOrCtrl+M'}
         ]
     },
 ];
@@ -28,7 +28,9 @@ if (process.platform === 'darwin') {
     template.unshift({
         label: app.getName(),
         submenu: [
-            {role: 'quit', accelerator: 'Cmd+Q'}
+            {role: 'preferences', label: 'Preferences', accelerator: 'Cmd+,'},
+            {type: 'separator'},
+            {role: 'quit', label: 'Quit', accelerator: 'Cmd+Q'}
         ]
     });
 }
