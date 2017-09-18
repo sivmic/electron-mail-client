81b19bd99cc50c660611d2a48c1a3f089ac619a1
sivmic <sivak.dacice@outlook.com>
2017-09-18 20:50:32 +0200

    Menu accelerators implemented

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,42 +1,36 @@
 const {app, Menu} = require('electron');
 
 const template = [
-  {
-    label: 'Edit',
-    submenu: [
-      {role: 'undo'},
-      {role: 'redo'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {role: 'reload'},
-      {role: 'toggledevtools'}
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {role: 'minimize'},
-      {role: 'close'}
-    ]
-  },
+    {
+        label: 'Edit',
+        submenu: [
+            {role: 'undo', accelerator: 'CmdOrCtrl+Z'},
+            {role: 'redo', accelerator: 'Shift+CmdOrCtrl+Z'}
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {role: 'reload', accelerator: 'CmdOrCtrl+R'},
+            {role: 'toggledevtools', accelerator: 'Alt+CmdOrCtrl+I'}
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            {role: 'close', accelerator: 'CmdOrCtrl+W'},
+            {role: 'minimize', accelerator: 'CmdOrCtrl+M'}
+        ]
+    },
 ];
 
 if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {role: 'quit'}
-    ]
-  });
-
-  // Window menu
-  template[3].submenu = [
-    {role: 'close'},
-    {role: 'minimize'}
-  ]
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            {role: 'quit', accelerator: 'Cmd+Q'}
+        ]
+    });
 }
 
 const menu = Menu.buildFromTemplate(template);
