92929235151ec88293a80c2e3127924ae27753e5
sivmic <sivak.dacice@outlook.com>
2017-09-19 16:03:01 +0200

    Added blocking zooming in app

--- renderer.js
+++ renderer.js
@@ -1,6 +1,11 @@
 const ipcRenderer = require('electron').ipcRenderer;
+const webFrame = require('electron').webFrame;
+
 
 // Listener of clicks on #compose button, when clicked, ipcRenderer send 'compose' signal to ipcMain in /main.js
 document.querySelector('#compose').addEventListener('click', () => {
     ipcRenderer.send('compose')
 });
+
+// Blocking zooming in app
+webFrame.setZoomLevelLimits(1, 1);
